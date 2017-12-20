f09e85ec7f21ce6940e5bbb8fae2f884525e8dee
NAO-Projekt-DHBW <34681716+NAO-Projekt-DHBW@users.noreply.github.com>
2017-12-20 13:59:48 +0100

    Kreise hinzugefügt

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,6 +5,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.shape.Ellipse;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,6 +14,8 @@ public class Controller implements Initializable {
     public Label labelname;
     public TextField textname;
     public Button buttonname;
+    public Ellipse ellipse;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         
