b0922e3c5ed26523caf55b91a45e06054ec38395
NAO-Projekt-DHBW <34681716+NAO-Projekt-DHBW@users.noreply.github.com>
2017-12-20 13:44:21 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    public Label labelname;
+    public TextField textname;
+    public Button buttonname;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+
+    public void Message(ActionEvent actionEvent) {
+        System.out.println("Test");
+    }
+}
