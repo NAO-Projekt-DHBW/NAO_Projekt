f89e07839e4bc6ecd9772e8a45c17a751b6cd7b9
NAO-Projekt-DHBW <34681716+NAO-Projekt-DHBW@users.noreply.github.com>
2018-01-10 13:45:38 +0100

    javaFX -  Klassen: Sound + Images

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,9 +2,8 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.shape.Ellipse;
 
 import java.net.URL;
@@ -13,6 +12,15 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
     public Button buttonwiedergabe;
+    public Label labelsound;
+    public Label labelsoundinfo;
+    public TextField fieldsound;
+    public ComboBox comboboxsprache;
+    public ComboBox comboboxsprachgeschwindigkeit;
+    public Slider reglertonhoehe;
+    public Label labeltonhoehe;
+    public ImageView bildnao;
+    public ImageView bildsound;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
