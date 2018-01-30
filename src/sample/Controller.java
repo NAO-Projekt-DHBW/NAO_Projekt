19d723d6bf14d7ae3d236c7d6ef05d5e58792f40
Timon <tdages@posteo.de>
2018-01-30 09:41:52 +0100

    - Neue Methode "writeConnectionToFile": Abspeichern der letzten (erfolgreich genutzten) Verbindungsdaten in einer Datei connection.txt (die Datei liegt vorerst im IntelliJ-Projektverzeichnis). IP-Adresse und Port werden in einer Zeile geschrieben. es zweidimensionalen Arrays (mit Strings), welche IP, Port und Farbe der NAOs beinhalten. Aus diesem werden dann die Items für das Dropdown-Feld zum Auswählen der Verbindung gesetzt. - Neue Methode: Beim Starten des Programms wird Datei "connections.txt" eingelesen und die Verbindungsdaten in das zweidimensionale Array gespeichert. - Erstellen ein - Hinzufügen eines Buttons um die aktuelle Verbindung zu trennen - Hinzufügen einer "Ampel" für den Verbindungsstatus. Farbe wird grün bei erfolgreichem Verbindungsaufbau und wieder rot wenn es getrennt wird -Neue Methode "clearFieldsAfterConnecting", welche beim Verbinden/Neuverbinden mit einem NAO ausgeführt wird. Hier können Textfelder eingetragen werden, die beim Verbinden geleert werden sollen.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,5 @@
 package sample;
 
-import com.aldebaran.qi.Application;
 import com.aldebaran.qi.Session;
 import com.aldebaran.qi.helper.proxies.*;
 import javafx.collections.FXCollections;
@@ -12,33 +11,35 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.shape.Ellipse;
-import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
+import javax.swing.*;
+import java.io.*;
 import java.net.URL;
 import java.util.*;
 
 public class Controller implements Initializable {
 
-    public Button btnwiedergabe;
-    public Label labelsound;
+    public Button btnPlay;
+    public Label labelSound;
     public Label labelsoundinfo;
-    public TextField fieldsound;
-    public ComboBox comboboxsprache;
+    public TextField fieldSound;
+    public ComboBox comboBoxLanguage;
     public ComboBox comboboxsprachgeschwindigkeit;
-    public Slider reglertonhoehe;
-    public Label labeltonhoehe;
-    public ImageView bildnao;
-    public ImageView bildsound;
-    public ComboBox ComboBoxAugenLEDFarbe;
-    public ComboBox ComboBoxOhrenLEDFarbe;
-    public Button btnBlinken;
+    public Slider sliderPitch;
+    public Label labelPitch;
+    public ImageView imgNAO;
+    public ImageView imgBubble;
+    public ComboBox comboBoxEyesLEDColour;
+    public ComboBox comboBoxEarsLEDColour;
+    public Button btnBlink;
     public Button btnZwinkern;
-    public Button btnRuecken;
-    public ComboBox ComboBoxNAOWaehlen;
-    public Button btnVerbinden;
-    public TextField FieldIPAdresse;
-    public TextField FieldPortAnpassen;
+    public Button btnLyingBack;
+    public ComboBox comboBoxSelectNAO;
+    public Button btnConnect;
+    public TextField fieldIPAdress;
+    public TextField fieldPort;
     public Button btnW;
     public Button btnS;
     public Button btnA;
@@ -49,13 +50,13 @@ public class Controller implements Initializable {
     public Button btnRight;
     public Button btnLeft;
     public Button btnDown;
-    public Button btnBauch;
+    public Button btnLyingBelly;
     public Button btnWinken;
-    public Button btnLEDRuecken;
-    public Button btnHocken;
-    public Button btnStuhlsitzen;
-    public Button btnRelaxen;
-    public Label labelLaufen;
+    public Button btnBackLED;
+    public Button btnCrouch;
+    public Button btnSitChair;
+    public Button btnSitRelax;
+    public Label labelWalk;
     public ToggleButton toggleRest;
     public ToggleButton toggleWakeUp;
     public Button btnTaiChi;
@@ -65,74 +66,133 @@ public class Controller implements Initializable {
     public TextField fieldTemperature;
     public Slider sliderPace;
     public TextArea fieldConnectionState;
-    public Button btnAufstehen;
-    public Button btnSitzen;
+    public Button btnStand;
+    public Button btnSit;
+    public Button btnCloseConnection;
 
-    private static String ipAdress;
+    private static String[][] arrayNAO = new String[4][3];
     private static String defaultPort = "9559";
+    private static String fileLastConnection = "connection.txt";
     private static Session session = new Session();
-    private static Float speachPitch = 0f;
-
+    private static Float defaultSpeechPitch = 0f;
+    public Circle circleConnectionState;
 
     //Alles was unter dieser Methode steht, wird direkt beim Starten des Programms ausfrüht.
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-        List<String> supplierNames1 = new ArrayList<String>();
-        supplierNames1.add(0, "192.168.178.1 (blau)");
-        supplierNames1.add(1, "192.168.178.2 (rot)");
-        supplierNames1.add(2, "192.168.178.3 (grün)");
-        ComboBoxNAOWaehlen.setItems(FXCollections.observableArrayList(supplierNames1));
-        comboboxsprache.setItems(FXCollections.observableArrayList("Deutsch", "Englisch"));
-        comboboxsprache.getSelectionModel().selectFirst();
+        arrayNAO[0][0] = "192.168.178.1";
+        arrayNAO[0][1] = "blau";
+        arrayNAO[0][2] = defaultPort;
+
+        arrayNAO[1][0] = "192.168.178.2";
+        arrayNAO[1][1] = "rot";
+        arrayNAO[1][2] = defaultPort;
+
+        arrayNAO[2][0] = "192.168.178.3";
+        arrayNAO[2][1] = "grün";
+        arrayNAO[2][2] = defaultPort;
+
+        try {
+            String temp = getLastConnectionFromFile();
+            String[] split = temp.split(";");
+            arrayNAO[3][0] = split[0];
+            arrayNAO[3][1] = "letzte Verbindung";
+            arrayNAO[3][2] = split[1];
+        } catch (Exception e) {
+        }
+
+        List<String> listNAO = new ArrayList<String>();
+        for (int i = 0; i < arrayNAO.length; i++){
+            if(arrayNAO[i][0] != null) {
+                listNAO.add(i, arrayNAO[i][0] + " (" + arrayNAO[i][1] + ")");
+            }
+        }
+        comboBoxSelectNAO.setItems(FXCollections.observableArrayList(listNAO));
+        comboBoxLanguage.setItems(FXCollections.observableArrayList("Deutsch", "Englisch"));
+        comboBoxLanguage.getSelectionModel().selectFirst();
     }
 
-    public void setFieldIPAdresse(ActionEvent actionEvent){
-        FieldIPAdresse.clear();
-        FieldPortAnpassen.clear();
-        ipAdress = extractStringBefore(ComboBoxNAOWaehlen.getValue().toString()," ");
-        FieldIPAdresse.appendText(ipAdress);
-        FieldPortAnpassen.appendText(defaultPort);
+    public void setConnectionData(ActionEvent actionEvent) {
+        fieldIPAdress.clear();
+        fieldPort.clear();
+        int selected = comboBoxSelectNAO.getSelectionModel().getSelectedIndex();
+        fieldIPAdress.appendText(arrayNAO[selected][0]);
+        fieldPort.appendText(arrayNAO[selected][2]);
     }
 
     //Verbindung zum NAO aufbauen
     public void startConnection(ActionEvent actionEvent) throws Exception {
         //String robotUrl = "tcp://127.0.0.1:39513";
-        String robotUrl = "tcp://" + FieldIPAdresse.getText().toString() + ":" + FieldPortAnpassen.getText().toString();
+        String robotUrl = "tcp://" + fieldIPAdress.getText().toString() + ":" + fieldPort.getText().toString();
         try {
             session.connect(robotUrl).get();
             if (session.isConnected()) {
-                fieldConnectionState.clear();
-                fieldConnectionState.getStyleClass().add("success");
-                fieldConnectionState.appendText("Verbunden");
+                circleConnectionState.setFill(Color.GREEN);
+                writeConnectionToFile(fieldIPAdress.getText().toString(), fieldPort.getText().toString());
+                btnConnect.setDisable(true);
+                btnCloseConnection.setDisable(false);
+                clearFieldsAfterConnecting();
+                //fieldConnectionState.appendText("Verbunden");
                 fieldBattery.appendText(getBatteryState(actionEvent));
                 fieldTemperature.appendText(getTemperature(actionEvent));
                 //getLanguages(actionEvent);
-            } else {
-                fieldConnectionState.appendText("Verbindungsaufbau fehlgeschlagen.");
             }
-        } catch(Exception ex){
-            fieldConnectionState.clear();
-            fieldConnectionState.getStyleClass().add("error");
-            fieldConnectionState.appendText("Verbindungsaufbau fehlgeschlage. Url: " + robotUrl + ex);
+        } catch (Exception ex) {
+            clearFieldsAfterConnecting();
+            JOptionPane.showMessageDialog(null, ex.toString(), "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
-    public String getBatteryState(ActionEvent actionEvent) throws Exception{
+    public void stopConnection(ActionEvent actionEvent) throws Exception{
+        if(session.isConnected()) {
+            session.close();
+        }
+        if(!session.isConnected()){
+            circleConnectionState.setFill(Color.RED);
+            clearFieldsAfterConnecting();
+            btnCloseConnection.setDisable(true);
+            btnConnect.setDisable(false);
+        }
+    }
+
+    public String getLastConnectionFromFile() throws Exception{
+        String line = null;
+        File tempFile = new File(fileLastConnection);
+        if (tempFile.exists()) {
+            BufferedReader in = new BufferedReader(new FileReader(fileLastConnection));
+            line = in.readLine();
+            in.close();
+        }
+        return line;
+    }
+
+    public void writeConnectionToFile(String ip, String port) throws Exception {
+        PrintWriter writer = new PrintWriter(fileLastConnection, "UTF-8");
+        writer.println(fieldIPAdress.getText().toString() + ";" + fieldPort.getText().toString());
+        writer.close();
+    }
+
+    //diese Methode leert alle Textfelder, die beim Neuverbinden direkt wieder mit Text befüllt werden. Die Methode wird beim Aufbauen einer Verbindunge aufgerufen.
+    public void clearFieldsAfterConnecting() throws Exception{
+        fieldTemperature.clear();
+        fieldBattery.clear();
+    }
+
+    public String getBatteryState(ActionEvent actionEvent) throws Exception {
         ALBattery battery = new ALBattery(session);
         int state = battery.getBatteryCharge();
         return String.valueOf(state);
     }
 
-    public String getTemperature(ActionEvent actionEvent) throws Exception{
+    public String getTemperature(ActionEvent actionEvent) throws Exception {
         String result;
         ALBodyTemperature temperature = new ALBodyTemperature(session);
         Object temp = temperature.getTemperatureDiagnosis();
-        if(temp instanceof ArrayList){
-            ArrayList tempList = (ArrayList)temp;
+        if (temp instanceof ArrayList) {
+            ArrayList tempList = (ArrayList) temp;
             result = tempList.get(0).toString();
-        }
-        else{
+        } else {
             result = "N/A";
         }
         return result;
@@ -143,88 +203,78 @@ public class Controller implements Initializable {
         motion.wakeUp();
     }
 
-    public void rest (ActionEvent actionEvent) throws Exception {
+    public void rest(ActionEvent actionEvent) throws Exception {
         ALMotion motion = new ALMotion(session);
         motion.rest();
     }
 
-    public void movePace (ActionEvent actionEvent) throws Exception {
+    public void movePace(ActionEvent actionEvent) throws Exception {
 
     }
 
-    public void lookRight (ActionEvent actionEvent) throws Exception {
+    public void lookRight(ActionEvent actionEvent) throws Exception {
         ALMotion motion = new ALMotion(session);
         motion.angleInterpolationWithSpeed("HeadYaw", -0.5f, 0.45f);
     }
 
-    public void lookLeft (ActionEvent actionEvent) throws Exception {
+    public void lookLeft(ActionEvent actionEvent) throws Exception {
         ALMotion motion = new ALMotion(session);
         motion.angleInterpolationWithSpeed("HeadYaw", 0.5f, 0.45f);
     }
 
-    public void lookUp (ActionEvent actionEvent) throws Exception {
+    public void lookUp(ActionEvent actionEvent) throws Exception {
         ALMotion motion = new ALMotion(session);
         motion.angleInterpolationWithSpeed("HeadPitch", -0.5f, 0.45f);
     }
 
-    public void lookDown (ActionEvent actionEvent) throws Exception {
+    public void lookDown(ActionEvent actionEvent) throws Exception {
         ALMotion motion = new ALMotion(session);
         motion.angleInterpolationWithSpeed("HeadPitch", 0.5f, 0.45f);
     }
 
-    public void turnRight (ActionEvent actionEvent) throws Exception {
+    public void turnRight(ActionEvent actionEvent) throws Exception {
         ALMotion motion = new ALMotion(session);
         motion.moveTo(0f, 0f, 1f);
     }
 
-    public void turnLeft (ActionEvent actionEvent) throws Exception {
+    public void turnLeft(ActionEvent actionEvent) throws Exception {
         ALMotion motion = new ALMotion(session);
         motion.moveTo(0f, 0f, -1f);
     }
 
-    public void moveForward (ActionEvent actionEvent) throws Exception {
+    public void moveForward(ActionEvent actionEvent) throws Exception {
         ALMotion motion = new ALMotion(session);
         motion.moveTo(0.1f, 0f, 0f);
     }
 
-    public void moveBackwards (ActionEvent actionEvent) throws Exception {
+    public void moveBackwards(ActionEvent actionEvent) throws Exception {
         ALMotion motion = new ALMotion(session);
         motion.moveTo(-0.1f, 0f, 0f);
     }
 
-    public void moveLeft (ActionEvent actionEvent) throws Exception {
+    public void moveLeft(ActionEvent actionEvent) throws Exception {
         ALMotion motion = new ALMotion(session);
         motion.moveTo(0f, 0.1f, 0f);
     }
 
-    public void moveRight (ActionEvent actionEvent) throws Exception {
+    public void moveRight(ActionEvent actionEvent) throws Exception {
         ALMotion motion = new ALMotion(session);
         motion.moveTo(0f, -0.1f, 0f);
     }
 
-    //Sprechen
-    public void getLanguages(ActionEvent actionEvent) throws Exception {
-        ALTextToSpeech tts = new ALTextToSpeech(session);
-        String result;
-        result = tts.getAvailableLanguages().toString();
-        result += tts.getAvailableVoices().toString();
-        fieldsound.appendText(result);
-    }
-
     public void sayBubble(ActionEvent actionEvent) throws Exception {
         ALTextToSpeech tts = new ALTextToSpeech(session);
-        if(comboboxsprache.getValue().toString() == "Deutsch") {
+        if (comboBoxLanguage.getValue().toString() == "Deutsch") {
             tts.setLanguage("German");
         } else {
             tts.setLanguage("English");
         }
         tts.setVolume(1f);
-        if(reglertonhoehe.getValue() >= 1f)
-        {
-            speachPitch = (float) reglertonhoehe.getValue();
+        if (sliderPitch.getValue() >= 1f) {
+            defaultSpeechPitch = (float) sliderPitch.getValue();
         }
-        tts.setParameter(" pitchShift", speachPitch);
-        tts.say(fieldsound.getText());
+        tts.setParameter(" pitchShift", defaultSpeechPitch);
+        tts.say(fieldSound.getText());
     }
 
 
@@ -284,14 +334,5 @@ public class Controller implements Initializable {
     public void crouch(ActionEvent actionEvent) throws Exception {
         ALRobotPosture rp = new ALRobotPosture(session);
         Boolean success = rp.goToPosture("Crouch", 1f);
-    }
-
-    //Methode um einen String aus einem String zu extrahieren
-    public String extractStringBefore(String originalString, String stringToExtractBefore) {
-        int posA = originalString.indexOf(stringToExtractBefore);
-        if (posA == -1) {
-            return "";
-        }
-        return originalString.substring(0, posA);
     }
 }
